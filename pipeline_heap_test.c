6ee893dee17128624dfd49ab0bfcffe2789b83bd
NoahCherry <ncherry@bu.edu>
2024-01-23 18:05:15 +0000

    dynamically allocating pipeline commands and adding them to pipeline

---
+++ pipeline_heap_test.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+#include "myshell_parser.h"
+#include "stddef.h"
+
+int main()
+{
+  struct pipeline pl;
+
+  for(int i = 0; i < n; i++){
+    struct pipeline_command 
+  }
+
+}
