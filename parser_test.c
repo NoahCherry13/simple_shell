650834df9545490928a82c682bfadf0d5fdd53fe
NoahCherry <ncherry@bu.edu>
2024-01-25 17:01:43 +0000

    fixed issue where pipeline free was not propperly freeing pipeline

--- parser_test.c
+++ parser_test.c
@@ -5,16 +5,8 @@
 
 int main()
 {
-  char string[] = "hello                           a world";
+  char string[] = "";
   char *token = strtok(string, " ");
-
-  struct pipeline_command testpipe_command;
-  testpipe_command.command_args[0] = "ls";
-  testpipe_command.command_args[0] = "-a";
-  testpipe_command.redirect_in_path = "in.txt";
-  testpipe_command.redirect_out_path = "out.txt";
-
-  puts(testpipe_command.command_args[0]);
   
   while (token) {
     puts(token);
