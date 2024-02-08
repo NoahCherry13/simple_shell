5d104db9cab717bc678774f6fdd5147ca72c0784
NoahCherry <ncherry@bu.edu>
2024-02-08 18:36:23 +0000

    fixed pipeline free. Now frees duped strings for args and redirects

--- myshell_parser.c
+++ myshell_parser.c
@@ -5,7 +5,17 @@
 #include <stdlib.h>
 
 
-
+struct pipeline_command* allocate_cmd()
+{
+  struct pipeline_command *cmd = malloc(sizeof(struct pipeline_command));
+  if (cmd) {
+      cmd->command_args[0] = NULL;
+      cmd->next = NULL;
+      cmd->redirect_in_path = NULL;
+      cmd->redirect_out_path = NULL;
+    }
+    return cmd;
+}
 
 void get_args(char *token, const char *delim, struct pipeline_command *cmd, int *arg_num)
 {
@@ -19,43 +29,40 @@ void get_args(char *token, const char *delim, struct pipeline_command *cmd, int 
 
     if (is_cmd) {
       cmd->command_args[0] = strdup(arg_token);
+      is_cmd = 0;
+      //printf("COMMAND: %s\n", cmd->command_args[0]);
     }
     
     arg_token = strtok_r(NULL, " ", &arg_saveptr);
     while (arg_token != NULL){
-      cmd->command_args[*arg_num] = arg_token;
+      cmd->command_args[*arg_num] = strdup(arg_token);
+      //printf("ARG TOKEN: %s\nARG_NUM: %d\n", arg_token, *arg_num);
       *arg_num = *arg_num+1;
       arg_token = strtok_r(NULL, " ", &arg_saveptr);
     }
     outer_token = strtok_r(NULL, "<>", &outer_saveptr);
   }
+  //cmd->command_args[*arg_num] = NULL;
 
   return;
 }
-
-
-
-
 
 void check_redirect(char *token, const char *delim, struct pipeline_command *cmd)
 {
   char *redirect_saveptr = NULL;
-  char *redirect_token = strtok_r(token, "delim", &redirect_saveptr);
+  char *redirect_token = strtok_r(token, delim, &redirect_saveptr);
   redirect_token = strtok_r(NULL, " &<>", &redirect_saveptr);
-
+  //printf("REDIRECT TOKEN: %s\n", redirect_token);
   if (redirect_token == NULL){
     cmd->redirect_in_path = NULL;
     cmd->redirect_out_path = NULL;
   } else if (delim[0] == '<') {
-    cmd->redirect_in_path = redirect_token;
+    cmd->redirect_in_path = strdup(redirect_token);
   } else {
-    cmd->redirect_out_path = redirect_token;
+    cmd->redirect_out_path = strdup(redirect_token);
   }
   return;
 }
-
-
-
 
 
 struct pipeline *pipeline_build(const char *command_line)
@@ -71,15 +78,22 @@ struct pipeline *pipeline_build(const char *command_line)
   struct pipeline *pl = malloc(sizeof(struct pipeline));
   int arg_num = 1;
   command_line_cpy[strlen(command_line_cpy)-1] = '\0';
+  if(strchr(command_line_cpy, '&')){
+    pl->is_background = 1;
+    *strchr(command_line_cpy, '&') = ' ';
+  } else {
+    pl->is_background = 0;
+  }
   char *token = strtok_r(command_line_cpy, pipe_delim, &pipe_saveptr);
 
   
   while (token != NULL) {
-    arg_num = 0;
+    arg_num = 1;
     char *rin_cpy = strdup(token);
     char *rout_cpy = strdup(token);
     char *arg_cpy = strdup(token);
-    struct pipeline_command *cmd = malloc(sizeof(struct pipeline_command));
+    //printf("Copies: %s\n", rin_cpy);
+    struct pipeline_command *cmd = allocate_cmd();
 
     if(pl->commands == NULL){
       pl->commands = cmd;
@@ -90,7 +104,7 @@ struct pipeline *pipeline_build(const char *command_line)
       }
       cmd_ptr->next = cmd;
     }
-
+    
     check_redirect(rin_cpy, redirect_in_delim, cmd);
     check_redirect(rout_cpy, redirect_out_delim, cmd);
     get_args(arg_cpy, arg_delims, cmd, &arg_num);
@@ -112,6 +126,13 @@ void pipeline_free(struct pipeline *pipeline)
   while (pipeline->commands->next != NULL) {
     free_ptr = pipeline->commands;
     pipeline->commands = pipeline->commands->next;
+    int i = 0;
+    while(free_ptr->command_args[i] != NULL){
+      free(free_ptr->command_args[i]);
+      i++;
+      }
+    free(free_ptr->redirect_in_path);
+    free(free_ptr->redirect_out_path);
     free(free_ptr);
   }
   free(pipeline);
--- test_simple_pipe.c
+++ test_simple_pipe.c
@@ -4,10 +4,9 @@
 #include <string.h>
 #include <assert.h>
 
-int
-main(void)
+int main(void)
 {
-  struct pipeline* my_pipeline = pipeline_build("ls | cat\n");
+  struct pipeline* my_pipeline = pipeline_build("ls arg1 < file1 > file2 -l| cat helloworld\n");
   
   // Test that a pipeline was returned
   assert(my_pipeline != NULL);
@@ -16,11 +15,14 @@ main(void)
   
   // Test the parsed args
   assert(strcmp("ls", my_pipeline->commands->command_args[0]) == 0);
-  assert(my_pipeline->commands->command_args[1] == NULL);
-  
+  assert(strcmp(my_pipeline->commands->command_args[1], "arg1") == 0);
+  assert(strcmp(my_pipeline->commands->command_args[2], "-l") == 0);
+  assert(strcmp("cat", my_pipeline->commands->next->command_args[0]) == 0);
+  assert(strcmp(my_pipeline->commands->next->command_args[1], "helloworld") == 0);
+    
   // Test the redirect state
-  assert(my_pipeline->commands->redirect_in_path == NULL);
-  assert(my_pipeline->commands->redirect_out_path == NULL);
+  assert(strcmp(my_pipeline->commands->redirect_in_path, "file1") == 0);
+  assert(strcmp(my_pipeline->commands->redirect_out_path, "file2") == 0);
   
   // Test that there are multiple parsed command in the pipeline
   assert(my_pipeline->commands->next != NULL);
